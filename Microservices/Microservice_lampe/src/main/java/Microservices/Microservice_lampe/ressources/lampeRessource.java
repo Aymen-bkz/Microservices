dfb0a1fcb66eee271c10c21a3ddc1cae50bc295c
aymenbkzz <aboukezzata@hotmail.fr>
2022-01-25 04:03:44 +0100

    update

---
+++ Microservices/Microservice_lampe/src/main/java/Microservices/Microservice_lampe/ressources/lampeRessource.java
@@ -0,0 +1,89 @@
+package Microservices.Microservice_lampe.ressources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Microservices.Microservice_lampe.model.Lampe;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/lampes")
+public class lampeRessource {
+    private Lampe[] Lampes_tab = { new Lampe(1, 1), new Lampe(1, 2), new Lampe(2, 1),
+            new Lampe(2, 2) };
+
+    @GetMapping("/status")
+    public String Lampe_all() {
+        String msg = "";
+        for (Lampe Lampe : Lampes_tab) {
+            msg += "les Lampes " + Lampe.getId() + " de l'étage " + Lampe.getEtage() + ", salle " + Lampe.getSalle()
+                    + " sont ";
+            if (Lampe.isEtat())
+                msg += "allumé <br>";
+            else
+                msg += "éteinte <br>";
+        }
+        return msg;
+    }
+
+    @GetMapping("/all")
+	public List<Lampe> getAllLampe() {
+		List<Lampe> Lampe_list = new ArrayList<Lampe>();
+		for (Lampe Lampe : Lampes_tab) {
+			Lampe_list.add(Lampe);
+		}
+		return Lampe_list;
+	}
+
+    @GetMapping("/{id}")
+    public Lampe getLampeByID(@PathVariable int id) {
+        Lampe buff = new Lampe();
+        for (Lampe Lampe : Lampes_tab) {
+            if (Lampe.getId() == id) {
+                buff = Lampe;
+            }
+        }
+        return buff;
+    }
+
+    @GetMapping("/etages/{etage}")
+    public List<Lampe> getLampeEtage(@PathVariable int etage) {
+        List<Lampe> Lampe_list = new ArrayList<Lampe>();
+        for (Lampe Lampe : Lampes_tab) {
+            if (Lampe.getEtage() == etage)
+                Lampe_list.add(Lampe);
+        }
+        System.out.println(Lampe_list.toString());
+        return Lampe_list;
+    }
+
+    @GetMapping("/salles/{salle}")
+    public List<Lampe> getLampeSalle(@PathVariable int salle) {
+        List<Lampe> Lampe_list = new ArrayList<Lampe>();
+        for (Lampe Lampe : Lampes_tab) {
+            if (Lampe.getEtage() == salle)
+                Lampe_list.add(Lampe);
+        }
+        System.out.println(Lampe_list.toString());
+        return Lampe_list;
+    }
+
+    @PostMapping("/order/{id}/{etat}")
+    public Lampe setLampeOrder(@PathVariable int id, @PathVariable Boolean etat) {
+        Lampe buff = new Lampe();
+        for (Lampe Lampe : Lampes_tab) {
+            if (Lampe.getId() == id) {
+                Lampe.setEtat(etat);
+                buff = Lampe;
+            }
+        }
+        return buff;
+    }
+}
---
+++ Microservices/Microservice_presence/src/main/java/Microservices/Microservice_presence/ressources/presenceRessource.java
@@ -0,0 +1,77 @@
+package Microservices.Microservice_presence.ressources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Microservices.Microservice_presence.model.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/presence")
+public class presenceRessource {
+
+	private static Presence[] tabPresence = { new Presence(1, 1), new Presence(1, 2),
+			new Presence(2, 1), new Presence(2, 2) };
+
+	@GetMapping("/{id}")
+	public Presence getState_id(@PathVariable int id) {
+		Presence buff = new Presence();
+		for (Presence presence : tabPresence) {
+			presence.update();
+			if (presence.getId() == id)
+				buff = presence;
+		}
+		return buff;
+	}
+
+	@GetMapping("/status")
+	public String getState_all() {
+		String msg = "";
+		for (Presence presence : tabPresence) {
+			msg += "le capteur de présence " + presence.getId() + " de l'étage " + presence.getEtage() + ", salle "
+					+ presence.getSalle() + " est " + presence.isEtat() + "<br>";
+		}
+		return msg;
+	}
+
+	@GetMapping("/all")
+	public List<Presence> getAllPresence() {
+		List<Presence> presence_list = new ArrayList<Presence>();
+		for (Presence presence : tabPresence) {
+			presence.update();
+			presence_list.add(presence);
+		}
+		return presence_list;
+	}
+
+	@GetMapping("/etage/{etage}")
+	public List<Presence> getState_Etage(@PathVariable int etage) {
+		List<Presence> presence_list = new ArrayList<Presence>();
+        for (Presence presence : tabPresence) {
+			presence.update();
+            if(presence.getEtage() == etage)
+                presence_list.add(presence);
+        }
+        System.out.println(presence_list.toString());
+        return presence_list;
+	}
+
+	@GetMapping("/salle/{salle}")
+	public List<Presence> getState_Salle(@PathVariable int salle) {
+		List<Presence> Presence_list = new ArrayList<Presence>();
+        for (Presence presence : tabPresence) {
+			presence.update();
+            if(presence.getEtage() == salle)
+                Presence_list.add(presence);
+        }
+        System.out.println(Presence_list.toString());
+        return Presence_list;
+	}
+
+}
